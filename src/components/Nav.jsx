a248a59ee01b68d897439f92068c34a3d221d912
PratapAyush <ayushpratapsingh235@gmail.com>
2022-05-21 23:57:25 +0530

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Mobile = () => {
+    return (
+        
+        <nav class="navfirst"> 
+           <div class="box">
+               <div class="box1">
+                   <h1>LOGOBACKREY</h1>
+               </div>
+               <div class="box2">
+                   <h3>Services</h3>
+                   <h3>project</h3>
+                   <h3>About</h3>
+               </div>
+               <div class="box3">
+                   <button>Contact</button>
+               </div>
+           </div>
+            </nav>
+        
+        
+        
+
+    ) 
+    
+};
+
+export default Mobile;
